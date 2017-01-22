07cd7e145d46c563e59dfd5749f10d9a82e7f4fd
Marshall Pierce <marshall@mpierce.org>
2017-01-22 11:49:47 -0800

    Deserialize date and time with tag prefix

--- src/lib.rs
+++ src/lib.rs
@@ -29,15 +29,30 @@ pub trait SubSecond {
 }
 
 pub trait Offset {
-    fn offset(&self) -> Option<u8>;
+    /// UTC offset, if specified.
+    /// The offset may be "elsewhere", meaning that this temporal value
+    /// is not at UTC but the timezone is not specified here, or it may
+    /// be specified as the number of 15-minute increments away from UTC
+    /// plus 64 (to make it always positive). Thus, UTC would be 64 and
+    /// UTC+2:00 would be 64 + 8 = 72.
+    fn offset(&self) -> Option<OffsetData>;
 }
 
+pub enum OffsetData {
+    SpecifiedElsewhere,
+    UtcOffset(u8)
+}
+
+#[derive(Debug)]
 pub struct DateOnly<'a> {
-    data: &'a [u8; 3]
+    // 3-bit tag, 12-bit year, 4-bit month, 5-bit day
+    data: &'a [u8]
 }
 
+#[derive(Debug)]
 pub struct TimeOnly<'a> {
-    data: &'a [u8; 3]
+    // 7-bit tag, 5-bit hour, 6-bit minute, 6-bit second
+    data: &'a [u8]
 }
 
 pub struct DateTime {}
@@ -48,12 +63,58 @@ pub struct DateTimeSecond {}
 
 pub struct DateTimeSecondOffset {}
 
+enum TypeTag {
+    DateOnly,
+    TimeOnly,
+    DateTime,
+    DateTimeOffset,
+    DateTimeSubsecond,
+    DateTimeSubsecondOffset
+}
+
+impl TypeTag {
+    fn matches(&self, byte: u8) -> bool {
+        let top_three_bits = 0b1110_0000;
+        let top_two_bits = 0b1100_0000;
+        let top_seven_bits = 0b1111_1110;
+        match self {
+            &TypeTag::DateOnly => byte & top_three_bits == 0b1000_0000,
+            &TypeTag::TimeOnly => byte & top_seven_bits == 0b1010_0000,
+            &TypeTag::DateTime => byte & top_two_bits == 0b0000_0000,
+            &TypeTag::DateTimeOffset => byte & top_three_bits == top_two_bits,
+            &TypeTag::DateTimeSubsecond => byte & top_two_bits == 0b0100_0000,
+            &TypeTag::DateTimeSubsecondOffset => byte & top_three_bits == top_three_bits
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum DeserError {
+    InputTooShort,
+    WrongTag
+}
+
+impl<'a> DateOnly<'a> {
+    pub fn from_slice(slice: &[u8]) -> Result<DateOnly, DeserError> {
+        if slice.len() < 3 {
+            return Err(DeserError::InputTooShort);
+        }
+
+        if !TypeTag::DateOnly.matches(slice[0]) {
+            return Err(DeserError::WrongTag);
+        }
+
+        Ok(DateOnly {
+            data: &slice[0..3]
+        })
+    }
+}
+
 impl<'a> Date for DateOnly<'a> {
     fn year(&self) -> Option<u16> {
-        // first 12 BE bits
-        let mut year = (self.data[0] as u16) << 4;
-        year |= (self.data[1] as u16) >> 4;
-        println!("year: {:X}", year);
+        // bits 4-15
+        let mut year = ((self.data[0] & 0x1F) as u16) << 7;
+        year |= (self.data[1] as u16) >> 1;
 
         if year == 4095 {
             None
@@ -63,8 +124,11 @@ impl<'a> Date for DateOnly<'a> {
     }
 
     fn month(&self) -> Option<u8> {
-        // bits 13-16
-        let month = self.data[1] & 0x0F;
+        // bits 16-19
+        let mut month = (self.data[1] & 0x01) << 3;
+        println!("month: {:02x}", month);
+        month |= (self.data[2] & 0xE0) >> 5;
+        println!("month: {:02x}", month);
 
         if month == 15 {
             None
@@ -74,7 +138,8 @@ impl<'a> Date for DateOnly<'a> {
     }
 
     fn day(&self) -> Option<u8> {
-        let day = self.data[2] >> 3;
+        // bits 20-24
+        let day = self.data[2] & 0x1F;
 
         if day == 31 {
             None
@@ -84,10 +149,27 @@ impl<'a> Date for DateOnly<'a> {
     }
 }
 
+impl<'a> TimeOnly<'a> {
+
+    pub fn from_slice(slice: &[u8]) -> Result<TimeOnly, DeserError> {
+        if slice.len() < 3 {
+            return Err(DeserError::InputTooShort);
+        }
+
+        if !TypeTag::TimeOnly.matches(slice[0]) {
+            return Err(DeserError::WrongTag);
+        }
+
+        Ok(TimeOnly {
+            data: &slice[0..3]
+        })
+    }}
+
 impl<'a> Time for TimeOnly<'a> {
     fn hour(&self) -> Option<u8> {
-        // first 5 bits
-        let hour = self.data[0] >> 3;
+        // bits 8-12
+        let mut hour = self.data[0] << 4;
+        hour |= (self.data[1] & 0xF0) >> 4;
 
         if hour == 31 {
             None
@@ -97,9 +179,9 @@ impl<'a> Time for TimeOnly<'a> {
     }
 
     fn minute(&self) -> Option<u8> {
-        // bits 6-11: bottom 3 of byte 0, top 3 of byte 1
-        let mut minute = (self.data[0] & 0x07) << 3;
-        minute |= self.data[1] >> 5;
+        // bits 13-18
+        let mut minute = (self.data[1] & 0x0F) << 2;
+        minute |= (self.data[2] & 0xC0) >> 6;
 
         if minute == 63 {
             None
@@ -109,9 +191,8 @@ impl<'a> Time for TimeOnly<'a> {
     }
 
     fn second(&self) -> Option<u8> {
-        // bits 12-17: bottom 5 of byte 1, top 1 of byte 2
-        let mut seconds = (self.data[1] & 0x1F) << 1;
-        seconds |= self.data[2] >> 7;
+        // bits 19-24
+        let seconds = self.data[2] & 0x3F;
 
         if seconds == 63 {
             None
@@ -127,8 +208,8 @@ mod tests {
 
     #[test]
     fn deser_date_all_missing() {
-        let bytes = &[0xFF, 0xFF, 0xFF];
-        let d = DateOnly { data: bytes };
+        let bytes = &[0x9F, 0xFF, 0xFF];
+        let d = DateOnly::from_slice(bytes).unwrap();
         assert_eq!(None, d.year());
         assert_eq!(None, d.month());
         assert_eq!(None, d.day());
@@ -136,17 +217,29 @@ mod tests {
 
     #[test]
     fn deser_date_none_missing() {
-        let bytes = &[0x7B, 0xF0, 0x70];
-        let d = DateOnly { data: bytes };
+        let bytes = &[0x8F, 0x7E, 0x0E];
+        let d = DateOnly::from_slice(bytes).unwrap();
         assert_eq!(Some(1983), d.year());
         assert_eq!(Some(1), d.month());
         assert_eq!(Some(15), d.day());
     }
 
     #[test]
+    fn deser_date_wrong_tag() {
+        let bytes = &[0xAF, 0xFF, 0xFF];
+        assert_eq!(DeserError::WrongTag, DateOnly::from_slice(bytes).unwrap_err());
+    }
+
+    #[test]
+    fn deser_date_too_short() {
+        let bytes = &[0xAF, 0xFF];
+        assert_eq!(DeserError::InputTooShort, DateOnly::from_slice(bytes).unwrap_err());
+    }
+
+    #[test]
     fn deser_time_all_missing() {
-        let bytes = &[0xFF, 0xFF, 0xFF];
-        let t = TimeOnly{ data: bytes };
+        let bytes = &[0xA1, 0xFF, 0xFF];
+        let t = TimeOnly::from_slice(bytes).unwrap();
         assert_eq!(None, t.hour());
         assert_eq!(None, t.minute());
         assert_eq!(None, t.second());
@@ -154,10 +247,23 @@ mod tests {
 
     #[test]
     fn deser_time_none_missing() {
-        let bytes = &[0x93, 0x26, 0x00];
-        let t = TimeOnly { data: bytes };
+        let bytes = &[0xA1, 0x26, 0x4C];
+        let t = TimeOnly::from_slice(bytes).unwrap();
         assert_eq!(Some(18), t.hour());
         assert_eq!(Some(25), t.minute());
         assert_eq!(Some(12), t.second());
     }
+
+    #[test]
+    fn deser_time_wrong_tag() {
+        let bytes = &[0xA3, 0xFF, 0xFF];
+        assert_eq!(DeserError::WrongTag, TimeOnly::from_slice(bytes).unwrap_err());
+    }
+
+    #[test]
+    fn deser_time_too_short() {
+        let bytes = &[0xAF, 0xFF];
+        assert_eq!(DeserError::InputTooShort, TimeOnly::from_slice(bytes).unwrap_err());
+    }
+
 }
