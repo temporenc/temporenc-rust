temporenc/temporenc-rust

commit 3b863104f5ee64adbbb73668c913bb82286fcaa0

Remove unneeded buffers from deser tests

src/date_only.rs
@@ -28,7 +28,6 @@ impl DateOnly {
         let mut raw_year = ((byte0 & 0x1F) as u16) << 7;
         let byte1 = buf[1];
         raw_year |= (byte1 as u16) >> 1;
-
         let year = if raw_year == YEAR_RAW_NONE {
             None
         } else {
@@ -39,20 +38,18 @@ impl DateOnly {
         let mut raw_month = (byte1 & 0x01) << 3;
         let byte2 = buf[2];
         raw_month |= (byte2 & 0xE0) >> 5;
-
         let month = if raw_month == MONTH_RAW_NONE {
             None
         } else {
-            Some(raw_month + 1)
+            Some(raw_month)
         };
 
         // bits 20-24
         let raw_day = byte2 & 0x1F;
-
         let day = if raw_day == DAY_RAW_NONE {
             None
         } else {
-            Some(raw_day + 1)
+            Some(raw_day)
         };
 
         // TODO check types that don't saturate full range (e.g. month)

benches/single_caller_deserialize_random.rs
@@ -22,11 +22,6 @@ fn deserialize_date_only_random(b: &mut Bencher) {
 
     b.bytes = v.len() as u64;
 
-    let mut buf = Vec::new();
-    for _ in 0..DateOnly::max_serialized_size() {
-        buf.push(0);
-    }
-
     b.iter(|| {
         let mut cursor = Cursor::new(v.as_slice());
         for _ in 0..NUM_ITEMS {
@@ -45,11 +40,6 @@ fn deserialize_time_only_random(b: &mut Bencher) {
     }
 
     b.bytes = v.len() as u64;
-
-    let mut buf = Vec::new();
-    for _ in 0..TimeOnly::max_serialized_size() {
-        buf.push(0);
-    }
 
     b.iter(|| {
         let mut cursor = Cursor::new(v.as_slice());
@@ -71,11 +61,6 @@ fn deserialize_date_time_random(b: &mut Bencher) {
 
     b.bytes = v.len() as u64;
 
-    let mut buf = Vec::new();
-    for _ in 0..DateTime::max_serialized_size() {
-        buf.push(0);
-    }
-
     b.iter(|| {
         let mut cursor = Cursor::new(v.as_slice());
         for _ in 0..NUM_ITEMS {
@@ -95,11 +80,6 @@ fn deserialize_date_time_offset_random(b: &mut Bencher) {
     }
 
     b.bytes = v.len() as u64;
-
-    let mut buf = Vec::new();
-    for _ in 0..DateTimeOffset::max_serialized_size() {
-        buf.push(0);
-    }
 
     b.iter(|| {
         let mut cursor = Cursor::new(v.as_slice());
@@ -122,11 +102,6 @@ fn deserialize_date_time_subsecond_random(b: &mut Bencher) {
 
     b.bytes = v.len() as u64;
 
-    let mut buf = Vec::new();
-    for _ in 0..DateTimeSubSecond::max_serialized_size() {
-        buf.push(0);
-    }
-
     b.iter(|| {
         let mut cursor = Cursor::new(v.as_slice());
         for _ in 0..NUM_ITEMS {
@@ -147,11 +122,6 @@ fn deserialize_date_time_subsecond_offset_random(b: &mut Bencher) {
     }
 
     b.bytes = v.len() as u64;
-
-    let mut buf = Vec::new();
-    for _ in 0..DateTimeSubSecondOffset::max_serialized_size() {
-        buf.push(0);
-    }
 
     b.iter(|| {
         let mut cursor = Cursor::new(v.as_slice());

commit 75014c866533a603bf0857d900e0b0232b0da376

Fix typo introduced in 3b86310 that broke date deserialization

src/date_only.rs
@@ -41,7 +41,7 @@ impl DateOnly {
         let month = if raw_month == MONTH_RAW_NONE {
             None
         } else {
-            Some(raw_month)
+            Some(raw_month + 1)
         };
 
         // bits 20-24
@@ -49,7 +49,7 @@ impl DateOnly {
         let day = if raw_day == DAY_RAW_NONE {
             None
         } else {
-            Some(raw_day)
+            Some(raw_day + 1)
         };
 
         // TODO check types that don't saturate full range (e.g. month)